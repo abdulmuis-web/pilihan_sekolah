bafc892b5ca6737eb5b5e9b70008dafa460ab36d
andri-sudarmawijaya <andri.sudarmawijaya@gmail.com>
2019-05-11 10:47:51 +0700

    add generate sekolah

---
+++ src/Form/SekolahGenerateForm.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Drupal\pilihan_sekolah\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\pilihan_sekolah\Entity\PilihanSekolah;
+use Drupal\pilihan_sekolah\PilihanSekolahGenerate;
+use Drupal\jenis_sekolah\Entity\JenisSekolah;
+use Drupal\simulasi\Lorem;
+
+/**
+ * Class SekolahGenerateForm.
+ */
+class SekolahGenerateForm extends FormBase {
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'sekolah_generate_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Submit'),
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $ids = \Drupal::entityQuery('vilage')
+      //->sort('created', 'ASC')
+	  ->range('0','100')
+	  ->groupBy('district_id')
+      ->execute();
+    $batch = array(
+      'title' => t('Deleting Node...'),
+      'operations' => array(
+        array(
+          '\Drupal\pilihan_sekolah\PilihanSekolahGenerate::generateProcess',
+          array($ids)
+        ),
+      ),
+      'finished' => '\Drupal\pilihan_sekolah\PilihanSekolahGenerate::finishedCallback',
+    );
+    batch_set($batch);
+  }
+}
---
+++ src/PilihanSekolahGenerate.php
@@ -0,0 +1,91 @@
+<?php 
+
+namespace Drupal\pilihan_sekolah;
+use Drupal\pilihan_sekolah\Entity\PilihanSekolah;
+use Drupal\jenis_sekolah\Entity\JenisSekolah;
+use Drupal\simulasi\Lorem;
+
+
+class PilihanSekolahGenerate {
+  public static function generateProcess($ids, &$context){
+    $message = 'Generate pilihan sekolah...';
+    $results = array();
+    $pilihan_sekolah == 0;
+	
+	foreach ($ids as $id) {
+		
+      //$pilihan_sekolah = PilihanSekolah::create($id);
+      //$results[] = $pilihan_sekolah->save();
+	    $pilihan_sekolah++;
+	    $results[] = $pilihan_sekolah;
+		
+	  $database = \Drupal::database();
+      $transaction = $database->startTransaction();
+	  $jenis_sekolah = PilihanSekolahGenerate::getJenisSekolahOptions();
+	  $pid = array();
+	  $pid['name'] = $jenis_sekolah->label() .' '. Lorem::ipsum('2', '2');
+	  $pid['jenis_sekolah'] = $jenis_sekolah->id();
+	  $pid['vilage'] = $id;
+	  $pid['zona'] = substr($id, '0', '4');
+	  $pid['npsn'] = mt_rand('11111111', '99999999');
+	  
+	  
+	  if (PilihanSekolahGenerate::checkNisn($pid['npsn'])){
+		continue;
+	  }
+      try {
+		$pilihan_sekolah = PilihanSekolah::create((array)$pid);
+		$pilihan_sekolah->save();
+      }
+      catch (\Exception $e) {
+        $transaction->rollback();
+        $vilage = NULL;
+        watchdog_exception('pilihan_sekolah', $e, $e->getMessage());
+        throw new \Exception(  $e->getMessage(), $e->getCode(), $e->getPrevious());
+      }
+	  $sandbox['current']++;
+      usleep(2);
+	  
+    }
+    $context['message'] = $message;
+    $context['results'] = $results;
+  }
+  
+  public static function finishedCallback($success, $results, $operations) {
+    // The 'success' parameter means no fatal PHP errors were detected. All
+    // other error management should be handled using 'results'.
+    if ($success) {
+      $message = \Drupal::translation()->formatPlural(
+        count($results),
+        'One post processed.', '@count posts processed.'
+      );
+    }
+    else {
+      $message = t('Finished with an error.');
+    }
+    drupal_set_message($message);
+  }
+  /**
+   * {@inheritdoc}
+   */
+  static function getJenisSekolahOptions(){
+	$query = \Drupal::entityQuery('jenis_sekolah')
+	->execute();
+    $id = array_rand($query);
+	$result = JenisSekolah::load($id);
+	//return 'aaa';
+	return $result;
+  }
+  
+  static function checkNisn($npsn){
+	  $database = \Drupal::database();
+	  $results = $database->select('pilihan_sekolah', 's')
+		  ->fields('s', array('npsn'))
+		  ->condition('s.npsn', $npsn, '=')
+		  ->execute();
+	  return (bool)$records;
+  }
+  
+}
+
+
